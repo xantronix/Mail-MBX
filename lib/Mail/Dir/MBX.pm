71e9066f7172283c16c6a155911b9727cefbcbe9
Xan Tronix <xan@cpan.org>
2014-10-23 12:10:56 -0500

    Yay, things are looking nicer!

--- lib/Mail/Dir/MBX.pm
+++ lib/Mail/Dir/MBX.pm
@@ -3,25 +3,10 @@ package Mail::Dir::MBX;
 use strict;
 use warnings;
 
-use Time::Local ();
-use Mail::Dir   ();
+use Mail::Dir              ();
+use Mail::Dir::MBX::Header ();
 
 our @ISA = qw(Mail::Dir);
-
-my %MONTHS = (
-    'Jan' => 0,
-    'Feb' => 1,
-    'Mar' => 2,
-    'Apr' => 3,
-    'May' => 4,
-    'Jun' => 5,
-    'Jul' => 6,
-    'Aug' => 7,
-    'Sep' => 8,
-    'Oct' => 9,
-    'Nov' => 10,
-    'Dec' => 11
-);
 
 sub import_mbx_file {
     my ($self, $file) = @_;
@@ -51,8 +36,8 @@ sub import_mbx_file {
     }
 
     foreach my $n ( 0 .. 29 ) {
-        $line = readline($fh);
-        $line =~ s/\r\n//;
+        chomp($line = readline($fh));
+
         if ( $line ne '' ) {
             push( @keywords, $line );
         }
@@ -64,62 +49,19 @@ sub import_mbx_file {
     my $delivered = 0;
 
     while ( $line = readline($fh) ) {
-        #
-        # tidyoff -- perltidy would wreak havoc on this poor expression
-        #
-        my ( $date, $time, $metadata ) = split /\s+/, $line;
+        my $header = Mail::Dir::MBX::Header->parse($line);
 
-        my ( $day, $month, $year ) = (
-            $date =~ /^( \d|\d\d)-(\w{3})-(\d{4})$/
-        ) or die('Invalid syntax: Bad date');
-
-        my ( $hour, $minute, $second ) = (
-            $time =~ /^(\d{2}):(\d{2}):(\d{2})$/
-        ) or die('Invalid syntax: Bad timestamp');
-
-        my ( $tz, $size, $attributes ) = (
-            $metadata =~ /^([+\-]\d{4}),(\d+);(\S+)$/
-        ) or die('Invalid syntax: Bad metadata');
-
-        my ( $tzNegative, $tzHourOffset, $tzMinuteOffset ) = (
-            $tz =~ /^([+\-])(\d{2})(\d{2})$/
-        ) or die('Invalid syntax: Bad timezone offset');
-
-        my ( $unused, $hexFlags, $uid ) = (
-            $attributes
-                =~
-            /^([[:xdigit:]]{8})([[:xdigit:]]{4})-([[:xdigit:]]{8})$/
-        ) or die('Invalid syntax: Bad attributes');
-
-        if ( $uid eq '00000000' ) {
-            $lazyuid++;
+        if ($header->{'uid'} == 0) {
+            $header->{'uid'} = ++$lazyuid;
         }
-        else {
-            $lazyuid = hex($uid);
-        }
-
-        my $hexuid = sprintf( '%08x', $lazyuid );
-
-        my $flags = 
-            ( ( hex($hexFlags) & 0x1 ) ? 'S' : '' )
-          . ( ( hex($hexFlags) & 0x2 ) ? 'T' : '' )
-          . ( ( hex($hexFlags) & 0x4 ) ? 'F' : '' )
-          . ( ( hex($hexFlags) & 0x8 ) ? 'R' : '' );
-    
-        my $timestamp = Time::Local::timegm(
-            $second, $minute, $hour, $day + 0, $MONTHS{$month}, $year
-        ) + ( ( $tzNegative eq '-' ? 1 : -1 )
-          * ( $tzHourOffset * 60 + $tzMinuteOffset ) * 60 );
-
-        # tidyon
 
         my $start = tell($fh);
-        my $end   = $start + $size - 1;
+        my $end   = $start + $header->{'size'} - 1;
 
         my $message = $self->deliver(sub {
             my ($out) = @_;
 
-            my $remaining = $size;
+            my $remaining = $header->{'size'};
             my $chunk     = 4096;
 
             while ($remaining > 0) {
@@ -139,7 +81,7 @@ sub import_mbx_file {
             $delivered++;
         });
 
-        $message->mark($flags);
+        $message->mark($header->{'flags'});
     }
 
     close $fh;
