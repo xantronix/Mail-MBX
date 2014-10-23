a8fc0a328c87a29832aa3a264f07c3aef451e81a
Xan Tronix <xan@cpan.org>
2014-10-23 12:53:08 -0500

    perltidy

--- lib/Mail/Dir/MBX.pm
+++ lib/Mail/Dir/MBX.pm
@@ -9,18 +9,19 @@ use Mail::Dir::MBX::Header ();
 our @ISA = qw(Mail::Dir);
 
 sub import_mbx_file {
-    my ($self, $file) = @_;
+    my ( $self, $file ) = @_;
 
     open( my $fh, '<', $file ) or die("Unable to open mailbox file $file: $!");
 
     my $line = readline($fh);
 
     #
-    # If the first line of the file is empty, then 
+    # If the first line of the file is empty, then
     #
-    if (!defined $line) {
+    if ( !defined $line ) {
         return;
-    } elsif ( $line ne "*mbx*\r\n" ) {
+    }
+    elsif ( $line ne "*mbx*\r\n" ) {
         die("File $file is not an MBX file: $line");
     }
 
@@ -31,12 +32,13 @@ sub import_mbx_file {
 
     if ( $line =~ /^([[:xdigit:]]{8})([[:xdigit:]]{8})\r\n$/ ) {
         $uidvalidity = hex($1);
-    } else {
+    }
+    else {
         die("File $file has invalid UID line");
     }
 
-    foreach my $n ( 0 .. 29 ) {
-        chomp($line = readline($fh));
+    foreach ( 0 .. 29 ) {
+        chomp( $line = readline($fh) );
 
         if ( $line ne '' ) {
             push( @keywords, $line );
@@ -51,37 +53,39 @@ sub import_mbx_file {
     while ( $line = readline($fh) ) {
         my $header = Mail::Dir::MBX::Header->parse($line);
 
-        if ($header->{'uid'} == 0) {
+        if ( $header->{'uid'} == 0 ) {
             $header->{'uid'} = ++$lazyuid;
         }
 
         my $start = tell($fh);
         my $end   = $start + $header->{'size'} - 1;
 
-        my $message = $self->deliver(sub {
-            my ($out) = @_;
+        my $message = $self->deliver(
+            sub {
+                my ($out) = @_;
 
-            my $remaining = $header->{'size'};
-            my $chunk     = 4096;
+                my $remaining = $header->{'size'};
+                my $chunk     = 4096;
 
-            while ($remaining > 0) {
-                my $len = $chunk < $remaining? $chunk: $remaining;
+                while ( $remaining > 0 ) {
+                    my $len = $chunk < $remaining ? $chunk : $remaining;
 
-                my $readlen = read($fh, my $buf, $len);
+                    my $readlen = read( $fh, my $buf, $len );
 
-                if (!defined $readlen) {
-                    die("Error reading @{[tell $fh]} from file $file: $!");
+                    if ( !defined $readlen ) {
+                        die("Error reading @{[tell $fh]} from file $file: $!");
+                    }
+
+                    print {$out} $buf;
+
+                    $remaining -= $readlen;
                 }
 
-                print {$out} $buf;
-
-                $remaining -= $readlen;
+                $delivered++;
             }
+        );
 
-            $delivered++;
-        });
-
-        $message->mark($header->{'flags'});
+        $message->mark( $header->{'flags'} );
     }
 
     close $fh;
